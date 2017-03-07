fbd7349031396ab6b26bdf357136f8a7e83daf14
Sarah Graves Smith <smith.g.sarah@gmail.com>
2017-03-07 14:37:25 -0500

    basic javascript written to query github api for user organizations login and avatar

--- src/main.js
+++ src/main.js
@@ -12,6 +12,11 @@ promise.then(function responseHandler(response) {
   if (response.status > 199 && response.status < 300) {
     console.log("yay, we got here!!");
     console.log(response.status);
+    response.json().then(function logUserData(userData) {
+      userData.forEach(function logOrgs(organization) {
+        console.log(organization.login, organization.avatar_url);
+      });
+    });
   } else {
     console.log('Error!!!', response.status);
   }
