36293ec8eca5ff7c03a4e9cd890cb920c98c5e82
Sarah Graves Smith <smith.g.sarah@gmail.com>
2017-03-07 15:20:08 -0500

    Drew avatar and login from github api and connected them to the html for display on the webpage

--- src/main.js
+++ src/main.js
@@ -10,11 +10,21 @@ let promise = fetch(
 
 promise.then(function responseHandler(response) {
   if (response.status > 199 && response.status < 300) {
-    console.log("yay, we got here!!");
-    console.log(response.status);
+    // console.log("yay, we got here!!");
+    // console.log(response.status);
     response.json().then(function logUserData(userData) {
-      userData.forEach(function logOrgs(organization) {
-        console.log(organization.login, organization.avatar_url);
+      userData.forEach(function logOrgs(organization, index) {
+        // console.log(organization.login, organization.avatar_url);
+        let organizationLi = document.createElement('li');
+        let orgHeading = document.createElement('h2');
+        let orgAvatar = document.createElement('img');
+        organizationLi.setAttribute('id', 'organization'+index);
+        orgAvatar.setAttribute('src', organization.avatar_url);
+        orgHeading.innerText = organization.login;
+        document.querySelector('#organizations ul').appendChild(organizationLi);
+        document.querySelector('#organization'+index).appendChild(orgHeading);
+        document.querySelector('#organization'+index).appendChild(orgAvatar);
+
       });
     });
   } else {
